524984c0f95cfc59386b4163fb6ea651d5f5ebb4
Michael Washburn Jr <mdw7326@rit.edu>
2015-02-18 00:46:41 -0500

    Added functionality to update the hour/minute/second hands and added functionality to dim the lights and disable the second hand at night. My prototype is now working.

--- RGB-LED-Clock.ino
+++ RGB-LED-Clock.ino
@@ -26,22 +26,39 @@ const int pinGrouping = 12; /* Grouping: 1 == RGBRGB, 2 == RRGGBB, ...*/
 /******************************************************************************
  * Global Variables
  ******************************************************************************/
- //empty
+typedef struct{
+  byte r;
+  byte g;
+  byte b;
+}rgb;
+rgb Leds[12];
+/* Clock Hands */
+int hourLed = -1;
+int minuteLed = -1;
+int secondLed = -1;
  
 /******************************************************************************
  * Setup Method
  ******************************************************************************/
 void setup() {
-
   Serial.begin(9600);
+
+  /* Init Leds array */
+  for(int i = 0; i < 12; i++){
+    Leds[i].r = 0;
+    Leds[i].g = 0;
+    Leds[i].b = 0;
+  }
 
   /* Initialize ShiftPWM ******************************************************/
   ShiftPWM.SetAmountOfRegisters(numRegisters);
   ShiftPWM.SetPinGrouping(pinGrouping);
   ShiftPWM.Start(pwmFrequency, maxBrightness);
+  SetLeds();
   
   /* Initialize Time **********************************************************/
   setSyncProvider(requestSync);
+  Serial.println("Waiting for sync message");  
 
 }//End of setup
 
@@ -49,18 +66,16 @@ void setup() {
  * Main Loop
  ******************************************************************************/
 void loop() {
-  
   /* Sync the time if serial connection is active */
   if(Serial.available()){
+    Serial.println("Serial Available");
     processSyncMessage();
   }
   /* If the time is set */
-  if(timeStatus() == timeSet){
+  if(timeStatus() != timeNotSet){
     //Show the time
+    UpdateClockHands();
   }
-  
-  ShiftPWM.OneByOneFast();
-
 }//end of main loop
 
 /*****************************************************************************
@@ -68,33 +83,70 @@ void loop() {
  *****************************************************************************/
 
 /*---------------------------------------------------------------------------*
- * LED Functions
- *---------------------------------------------------------------------------*/
-
-/*
-Adjust the RGB values for their different resistances.  I.e., red is less
-resistive than green and blue, while green and blue are almost the same
-resistance.  Therefore, to obtain the color white, the power being drawn 
-by the red led should be less than that of green and blue.  
-Parameters:
-  int led_index - the led to set.
-  byte * r - the red byte
-  byte * g - the green byte
-  byte * b - the blue byte */
-void SetRGB(int led_index, byte r, byte g, byte b){
-  /*Adjust the values */
-  r *= .5;
-  g *= .75;
-  b *= 1;
-  /*Set the led */
-  ShiftPWM.SetRGB(led_index, r, g, b);
-}
-
-/*---------------------------------------------------------------------------*
  * Clock/Time Keeping Functions
  *---------------------------------------------------------------------------*/
+ 
+ /*
+ Returns true if it is the user defined "night" time. */
+ boolean IsNightTime(){
+   if(hour() >= 20 || hour() <= 8)
+   {
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+ }
+ 
+ /*
+ Updates each hand of the clock face to reflect the actual time. */
+ void UpdateClockHands(){
+   /* Set the multiplier to one if the current time is during the day */
+   byte brightness = 255;
+   /* Set brightness to 5 if it is night time */
+   if(IsNightTime())
+   {
+     brightness = 5;
+   }
+   
+   /* Update the hour hand --------------------------------------------------*/
+   /* If the old position needs to be blanked */
+   if(hourLed >= 0){
+     Leds[hourLed].r = 0;
+   }
+   /* Set the hour hand */
+   hourLed = hour() % 12;
+   Leds[hourLed].r = brightness;
+   
+   /* Update the minute hand ------------------------------------------------*/
+   /* If the old position needs to be blanked */
+   if(minuteLed >= 0)
+   {
+     Leds[minuteLed].g = 0;
+   }
+   /* Set the minute hand */
+   minuteLed = minute()/5;
+   Leds[minuteLed].g = brightness;
   
-void digitalClockDisplay(){
+   /* Update the seconds hand -----------------------------------------------*/
+   /* Dont display the seconds hand during night time */
+   if(!IsNightTime()){
+     /* If the old position needs to be blanks */
+     if(secondLed >= 0){
+       Leds[secondLed].b = 0;
+     }
+     /*Set the second hand */
+     secondLed = second()/5;
+     Leds[secondLed].b = brightness;
+   }
+  
+  SetLeds();
+}
+
+/* debug functions ----------------------------------------------------------*/
+
+void printClockDisplay(){
   // digital clock display of the time
   Serial.print(hour());
   printDigits(minute());
@@ -116,14 +168,18 @@ void printDigits(int digits){
   Serial.print(digits);
 }
 
+/* Time Sync Functions ------------------------------------------------------*/
+
 void processSyncMessage() {
   unsigned long pctime;
   const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
-
+  
   if(Serial.find(TIME_HEADER)) {
      pctime = Serial.parseInt();
      if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       Serial.println("Time Set");  
        setTime(pctime); // Sync Arduino clock to the time received on the serial port
+       adjustTime(-18000);
      }
   }
 }
@@ -132,6 +188,34 @@ time_t requestSync()
 {
   Serial.write(TIME_REQUEST);  
   return 0; // the time will be sent later in response to serial mesg
+}
+
+/*---------------------------------------------------------------------------*
+ * LED Functions
+ *---------------------------------------------------------------------------*/
+
+/*
+Adjust the RGB values for their different resistances.  I.e., red is less
+resistive than green and blue, while green and blue are almost the same
+resistance.  Therefore, to obtain the color white, the power being drawn 
+by the red led should be less than that of green and blue. After adjusting,
+set the led.
+Parameters:
+  int led_index - the led to set.
+  byte * r - the red byte
+  byte * g - the green byte
+  byte * b - the blue byte */
+void SetLeds(){
+  /* For each led */
+  for(int i = 0; i < 12; i++)
+  {
+    /* Adjust the values */
+    Leds[i].r *= .5;
+    Leds[i].g *= .75;
+    Leds[i].b *= 1;
+    /* Set the led */
+    ShiftPWM.SetRGB(i, Leds[i].r, Leds[i].g, Leds[i].b);
+  }
 }
 
 
